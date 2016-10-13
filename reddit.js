ed4df6bed48b0c8a6cd7d48effbb6c47f4c3ca29
Mary Scale <scalemary@gmail.com>
2016-10-13 15:25:55 +0000

    Wrote the reddit.js file with promises

--- reddit.js
+++ reddit.js
@@ -5,36 +5,10 @@ var request = require("request");
 /*
 This function should "return" the default homepage posts as an array of objects
 */
-function getHomepage() {
-  // Load reddit.com/.json and call back with the array of posts
-  return(
-      new Promise(function(resolve, reject){
-          request("https://www.reddit.com/.json", function(err, result){
-              if(err){
-                  reject(err);
-              }
-              else{
-                  try{
-                      var homepage = JSON.parse(result.body);
-                      homepage = homepage.data.children;
-                      console.log(homepage);
-                      resolve(homepage);
-                  }
-                  catch(error){
-                      reject(error);
-                  }
-              }
-          });
-      })
-    );
-}
+//Don't use new Promise everywhere
+//just put it in a function and call that
 
-/*
-This function should "return" the default homepage posts as an array of objects.
-In contrast to the `getHomepage` function, this one accepts a `sortingMethod` parameter.
-*/
-function getSortedHomepage(sortingMethod) {
-    var url = "https://www.reddit.com/" + sortingMethod + ".json";
+function getPromise(url){
     return(
         new Promise(function(resolve, reject){
             request(url, function(err, result){
@@ -42,19 +16,114 @@ function getSortedHomepage(sortingMethod) {
                     reject(err);
                 }
                 else{
-                    try{
-                        
-                    }
-                    catch(error){
-                        reject(error);
-                    }
+                    resolve(result);
                 }
             })
         })
-    );
+    )
+}
+//end of getPromise function
+    //this function makes a new promise for us so that 
+    //we don't have to keep making new promises all over the place
+
+
+function getHomepage(url) {
+  // Load reddit.com/.json and call back with the array of posts
+  return (
+      getPromise(url + ".json")
+      .then(function(homepageResult){
+        //   console.log(JSON.parse(homepageResult.body).data);
+          return JSON.parse(homepageResult.body).data.children;
+      })
+    )
+}
+//end of getHomepage promise function
+    //this function will be used in all others since it 
+    //JSON parses the data for us
+        //Remember DRY ;)
+
+
+function getSortedHomepage(url, sortingMethod) {
+    return getHomepage(url + sortingMethod)
+    .then(function(sortingResult){
+        // console.log(sortingResult);
+        return sortingResult;
+    });
   }
 // Load reddit.com/{sortingMethod}.json and call back with the array of posts
   // Check if the sorting method is valid based on the various Reddit sorting methods
+//Sorting Method refers to tabs on the reddit homepage, like hot, new, rising
+    //controversial, top, gilded, and promoted.
+    
+    
+function getSubreddit(url, subreddit){
+    return getHomepage(url + "r/" + subreddit)
+    //adding r/ so that we can access the subreddit data
+        //using our getHomepage promise function
+    .then(function(subredditResult){
+        return subredditResult;
+    });
+}
+//end of getSubreddit promise function
 
 
-module.exports.get = getHomepage();
+function getSortedSubreddit(url, subreddit, sortingMethod){
+    return getHomepage(url + "r/" + subreddit + "/" + sortingMethod)
+    //don't forget the "/"
+        //urls need them to separated directories
+    .then(function(sortedSubResults){
+        return sortedSubResults;
+    });
+}
+//end of getSortedSubreddit promise function
+
+
+function getSubreddits(url){
+    return getHomepage(url + "subreddits")
+    .then(function(subsResults){
+        return subsResults;
+    });
+}
+
+
+//Mods below!
+
+module.exports.homepage = getHomepage;
+//exporting getHomepage
+    //access with .homepage
+module.exports.sortedHomepage = getSortedHomepage;
+//exporting getSortedHomepage
+    //access with .sortedHomepage
+module.exports.subreddit = getSubreddit;
+//exporting getSubreddit
+    //access with .subreddit
+module.exports.sortedSubreddit = getSortedSubreddit;
+//exporting getSortedSubreddit
+    //access with .sortedSubreddit    
+module.exports.subreddits = getSubreddits;    
+//exporting getSubreddits function
+    //NOTE!
+    //this is different from the getSubreddit function
+    //subreddits is a special directory in and of itself
+    //access with .subreddits
+
+//exporting the functions as mods
+    //NOTE
+        //the functions don't have ()
+        //that is because we want to call them in our other file, not
+        //here. Right now we just need the value of our function as
+        //a function
+
+// getHomepage("https://www.reddit.com/");
+
+// getSortedHomepage("https://www.reddit.com/", "top")
+// .then(function(result){
+//     console.log(result);
+// })
+// .catch(function(err){
+//     console.log("there was an error" + err)
+// });
+
+
+
+
--- testing.js
+++ testing.js
@@ -1,4 +1,50 @@
-var homepage = require("./reddit.js");
-//user homepage.get to access the getHomepage function
+var reddit = require("./reddit.js");
+//use reddit.homepage to get the homepage function
+    //reddit.sortedHomepage to get the sorted homepage function
+    //reddit.subreddit to get the subreddit function
+    //reddit.sortedSubreddit to get the sortedSubreddit function
+    //reddit.subreddits to get the subreddits function
+    
+var url = "https://www.reddit.com/";
+//use the global url variable to easily access reddit apis without
+    //hassle
 
-homepage.get;
+reddit.subreddits(url)
+.then(function(results){
+    console.log(results[0]);
+})
+.catch(function(err){
+    console.log("Oh no!" + err);
+});
+
+    //testing subreddits mod
+
+// reddit.sortedSubreddit(url, "movies", "top")
+// .then(function(results){
+//     console.log(results[0]);
+// })
+// .catch(function(err){
+//     console.log("crap, an error happened" + err);
+// });
+
+    //testing .sortedSubreddit mod
+
+// reddit.subreddit(url, "movies")
+// .then(function(result){
+//     console.log(result[0]);
+// })
+// .catch(function(err){
+//     console.log("there was an error" + err);
+// })
+
+    //testing .subreddit mod
+
+// reddit.sortedHomepage(url, "top")
+// .then(function(result){
+//     console.log(result[0]);
+// })
+// .catch(function(err){
+//     console.log("there was an error" + err)
+// });
+
+    //testing the .sortedHomepage mod
